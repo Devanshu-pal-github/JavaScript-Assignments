eb209380493da2723c7b8c4e420e543d1cf70181
Devanshu-pal-github <devanshu4943@gmail.com>
2026-02-27 03:42:00 +0000

    completed the second question of data types assignment

--- MyPractice/DataTypesAssignment/practice.js
+++ MyPractice/DataTypesAssignment/practice.js
@@ -1,10 +1,13 @@
 function Converter(kilo){
     const meter = kilo * 1000
     const centimeter = meter * 10
-
     console.log("meter=", meter)
     console.log("centimeter=", centimeter)
-
 }
-
 Converter(2)
+
+function area(radius){
+    const Area = radius * radius * 3.14
+    console.log("Area =",Area)
+}
+area(2)
