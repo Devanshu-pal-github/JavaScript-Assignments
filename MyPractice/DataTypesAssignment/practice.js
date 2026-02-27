f9df965d3dbe8e407c2648702d116e9bd9bab257
Devanshu-pal-github <devanshu4943@gmail.com>
2026-02-27 03:34:04 +0000

    completed first question of datatypes assignment

---
+++ MyPractice/DataTypesAssignment/practice.js
@@ -0,0 +1,10 @@
+function Converter(kilo){
+    const meter = kilo * 1000
+    const centimeter = meter * 10
+
+    console.log("meter=", meter)
+    console.log("centimeter=", centimeter)
+
+}
+
+Converter(2)
